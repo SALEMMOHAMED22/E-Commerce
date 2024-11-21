d2c274afd709d49a4e45b46e2956f1d4a5c78e87
SALEMMOHAMED22 <salemmohamed7422@gmail.com>
2024-11-21 02:57:20 +0200

    master project

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){ //...
+    });
